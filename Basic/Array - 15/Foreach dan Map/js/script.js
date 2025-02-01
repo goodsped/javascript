a903fb38e15c050494f026edcff681fdff565fa7
abekolin <albertog4taz28@gmail.com>
2025-02-01 20:41:56 +0700

    add code about array

---
+++ Basic/Array - 15/Foreach dan Map/js/script.js
@@ -0,0 +1,38 @@
+// array method
+
+// foreach
+var angka = [10,3,4,5,6,2,7,9,8,1];
+var mhs = ['Abe', 'Aldino', 'Putra', 'Alfa', 'Clementino'];
+
+// ini loop biasa 
+console.log('Loop For');
+for(i = 0; i < angka.length; i++){
+    console.log(angka[i])
+}
+
+
+// ini loop foreach
+console.log('\nLop Foreach')
+angka.forEach(function(e){
+    console.log(e);    
+});
+console.log('\nloop foreach array mhs\n\n')
+mhs.forEach(function(e, i){
+    console.log(e+' Mahasiswa ke '+(i+1));    
+});
+
+// map
+console.log('\nloop menggunakan map\n')
+var number = [1,2,3,4,5];
+var map = number.map(function(e){
+    return e * 2;    
+});
+console.log(map.join(' - '));
+
+// sort
+console.log('\n menampilkan array dgn sort');
+angka.sort(function(a, b){
+    return a-b;
+});
+console.log(angka.join(' - '));
+
--- Basic/Array - 15/Manipulasi Array/js/script.js
+++ Basic/Array - 15/Manipulasi Array/js/script.js
@@ -27,8 +27,35 @@ for(var i = 0; i < array.length; i++){
 var a = ['Albert', 'Alfa', 'Clementino'];
 // console.log(a.join(', '))
 
-// 2. push & pop
-a.push('Duncan');
+// 2. push
+a.push('Duncan', 'Ned');
 console.log(a.join(', '))
 
 // 3. pop
+a.pop();
+a.pop();
+console.log(a.join(', '))
+
+// 4. unshift
+a.unshift('Ned');
+console.log(a.join(', '))
+
+// 5. shift
+a.shift('Ned');
+console.log(a.join(', '))
+
+
+// 6. splice
+// splice(indexAwal;, mauDihapusBerapa, elemenBaru1, elemenBaru2, dst..)
+a.splice(0,3,'Ned','Arya', 'Brandon', 'Jhon', 'Rob');
+console.log(a.join(', '))
+
+// 7. slice
+// slice(awal, akhir)
+var arr2 = a.slice(0,4);
+console.log(arr2.join(', '));
+
+var a = ['Alfa', 'Clementino', 'Albert'];
+console.log(a.join(', '));
+
+
