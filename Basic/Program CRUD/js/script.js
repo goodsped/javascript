6bf98adc468cb6a57b7071af8042a888522f798d
abekolin <albertog4taz28@gmail.com>
2025-02-02 14:58:56 +0700

    mengubah sedikit kode

--- Basic/Program CRUD/js/script.js
+++ Basic/Program CRUD/js/script.js
@@ -1,43 +1,52 @@
 var penumpang = [];
-var tambahPenumpang = function(namaPenumpang, penumpang){
+
+var tambahPenumpang = function(namaPenumpang) {
     let ganti = JSON.parse(sessionStorage.getItem('turun'));
-    if(penumpang.length == 0){
+
+    if (penumpang.length == 0) {
         penumpang.push(namaPenumpang);
-    }else{
-        if(penumpang.includes(namaPenumpang)){
-            console.log(namaPenumpang+ ' sudah ada di buz');}
-        else if(ganti !== -1){
-             penumpang.splice(ganti,0,namaPenumpang);
-             sessionStorage.clear();
-        }
-        else if(penumpang !== undefined){
+    } else {
+        if (penumpang.includes(namaPenumpang)) {
+            console.log(namaPenumpang + ' sudah ada di buz');
+        } else if (typeof ganti === 'number' && ganti !== -1) {
+            penumpang.splice(ganti, 0, namaPenumpang);
+            sessionStorage.removeItem('turun');
+        } else {
             penumpang.push(namaPenumpang);
         }
     }
-    console.log('Penumpang : '+penumpang.join(' - ')), console.log(penumpang)
-}
-var hapusPenumpang = function(namaPenumpang, penumpang){
-    console.log(namaPenumpang+' turun dari buz') 
+
+    console.log('Penumpang: ' + penumpang.join(' - '));
+    console.log(penumpang);
+};
+
+var hapusPenumpang = function(namaPenumpang) {
+    console.log(namaPenumpang + ' turun dari buz');
     var turun = penumpang.indexOf(namaPenumpang);
-    sessionStorage.setItem('turun', JSON.stringify(turun))
-    penumpang.splice(turun,1);
-    return console.log('Kursi no '+(turun+1)+' kosong'), console.log(penumpang);
-}
 
-var ubahPenumpang = function(namaPenumpang, penumpang){
-    console.log(namaPenumpang+' turun dari buz') 
-    var turun = penumpang.indexOf(namaPenumpang);
-    sessionStorage.setItem('turun', JSON.stringify(turun))
-    penumpang.splice(turun,1);
-    return console.log('Kursi no '+(turun+1)+' kosong'), console.log(penumpang);
-}
+    if (turun !== -1) {
+        sessionStorage.setItem('turun', JSON.stringify(turun));
+        penumpang.splice(turun, 1);
+        console.log('Kursi no ' + (turun + 1) + ' kosong');
+    } else {
+        console.log(namaPenumpang + ' tidak ditemukan dalam buz');
+    }
 
-tambahPenumpang('Alfa', penumpang);
-tambahPenumpang('Abe', penumpang);
-tambahPenumpang('Emen', penumpang);
+    console.log(penumpang);
+};
 
-// hapus
-hapusPenumpang('Emen', penumpang);
+// Uji coba kode
+tambahPenumpang('Alfa');
+tambahPenumpang('Emen');
+tambahPenumpang('Abe');
 
-// tambah
-tambahPenumpang('Aldino', penumpang);
+// // Hapus
+hapusPenumpang('Emen');
+hapusPenumpang('Alfa');
+// hapusPenumpang('Abe');
+
+// Tambah kembali ke posisi yang kosong
+tambahPenumpang('Aldino');
+// tambahPenumpang('Alfa');
+tambahPenumpang('Emen');
+// tambahPenumpang('Abe');
